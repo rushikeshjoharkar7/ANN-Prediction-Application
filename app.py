5b936ea807dec98e671d3a36def6fffe9216f368
Rushikesh Joharkar <88484461+rushikeshjoharkar7@users.noreply.github.com>
2023-02-16 12:51:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
 from graphs_page import show_graphs_page
+from data_page import show_data_page
+from Calculator import calculator
 
 st.sidebar.title('Explore More')
 
-page = st.sidebar.radio("Navigation",("Predict", "Yarn Properties","Graphical Representation"))
+page = st.sidebar.selectbox("Navigation",("Predict","Properties","Processed Data","Graphical Representation","Calculator"))
 
 
 if page == "Predict":
     show_predict_page()
-elif page == "Yarn Properties" :
+elif page == "Properties" :
     show_explore_page()
-else :
+elif page == "Processed Data":
+    show_data_page()
+elif page == "Calculator":
+    calculator()      
+else:
     show_graphs_page()
